e5eccc7ab754ac7106820f369fc33b7ba18c6c68
kukiat <kukiat1996@gmail.com>
2018-04-29 00:26:37 +0700

    use morgan & cors

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
-const app = express()
 const bodyParser = require('body-parser')
 const routes = require('./routes')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
 }))
+
+app.use(morgan('tiny'))
+
+const options = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  exposedHeaders: ['x-auth-token'] 
+}
+app.use(cors(options))
 
 app.use('/', routes)
 
